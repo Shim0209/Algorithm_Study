25717dde7be8c61b445a8698e297f3f515a604da
yeonseong shim <shimyseong@gmail.com>
2022-05-14 23:52:34 +0900

    Update 선택정렬 수정

--- c#/Sort/SelectionSort/SelectionSort.cs
+++ c#/Sort/SelectionSort/SelectionSort.cs
@@ -21,18 +21,24 @@ namespace SelectionSort
             int[] arr = new int[] { 4, 3, 5, 9, 2, 1, 7, 8, 6, 0 };
             int targetIndex, temp;
 
+            foreach (int i in arr)
+                Console.Write($"{i} ");
+            Console.WriteLine();
+
             // 배열의 마지막 값은 자연스럽게 정렬 되므로 배열크기 -1 만큼만 반복
-            for(int i = 0; i < arr.Length - 1; i++)
+            for (int i = 0; i < arr.Length - 1; i++)
             {
                 // 시작인덱스 저장
                 targetIndex = i;
-
                 // 비교인덱스는 시작인덱스 + 1에서 배열의 마지막까지 반복
                 for(int j = i + 1; j < arr.Length; j++)
                 {
                     // 시작인덱스 > 비교인덱스 : 오름차순, 시작인덱스 < 비교인덱스 : 내림차순
-                    if (arr[i] > arr[j])
+                    if (arr[targetIndex] > arr[j])
+                    {
                         targetIndex = j;
+                    }
+                        
                 }
 
                 if(targetIndex != i)
@@ -45,7 +51,7 @@ namespace SelectionSort
             }
 
             foreach(int i in arr)
-                Console.WriteLine(arr[i]);
+                Console.Write($"{i} ");
         }
     }
 }
